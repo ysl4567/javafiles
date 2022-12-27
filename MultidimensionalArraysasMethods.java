04df102cf667c399f9f6ad45190fd4d1d8e579a3
ysl4567 <117042067+ysl4567@users.noreply.github.com>
2022-12-27 15:38:36 -0800

    Delete MultidimensionalArraysasMethods.java

--- MultidimensionalArraysasMethods.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-import java.util.Arrays;
-public class MultidimensionalArraysasMethods{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        //int[][] m = getArray();
-        
-    }
-}
